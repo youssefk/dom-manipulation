2a0e491358c45ac3d603882de8b6cf085dd0b0ca
Youssef <youssefkhalifa@live.com>
2015-09-30 20:53:49 -0700

    Make text items removable

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,33 @@ $(document).ready(function() {
   $("button#hello").click(function () {
     $("ul#user").prepend("<li>Hello! <span class='clickable delete'>x</span></li>");
     $("ul#webpage").prepend("<li>Why hello there! <span class='clickable delete'>x</span></li>");
-    $("li").css('background-color', 'green');
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#goodbye").click(function () {
     $("ul#user").prepend("<li>Goodbye! <span class='clickable delete'>x</span></li>");
     $("ul#webpage").prepend("<li>Goodbye, dear user! <span class='clickable delete'>x</span></li>");
-    $("li").css('background-color', 'green');
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#stop").click(function () {
     $("ul#user").prepend("<li>Stop copying me! <span class='clickable delete'>x</span></li>");
     $("ul#webpage").prepend("<li>Pardon me, I meant no offense. <span class='clickable delete'>x</span></li>");
-    $("li").css('background-color', 'green');
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 });
